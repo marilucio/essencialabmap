0d1174b52ba7015358eda8be7e40c9a44b250aae
seu-nome-de-usuario-do-github <mafale@gmail.com>
2026-04-24 20:21:54 -0300

    chore: update Facebook Pixel ID from 1176389559234307 to 1147561227476007
    Update the Facebook Pixel tracking ID in both index.html and Obrigado.jsx to use the new pixel ID (1147561227476007). This change ensures proper tracking and analytics for the application.

--- src/pages/Obrigado.jsx
+++ src/pages/Obrigado.jsx
@@ -29,7 +29,7 @@ function Obrigado() {
 
     // fbq (shim) já existe aqui; as chamadas ficam em fila até o script carregar
     // @ts-ignore
-    window.fbq('init', '1176389559234307');
+    window.fbq('init', '1147561227476007');
     // @ts-ignore
     window.fbq('track', 'PageView');
 
